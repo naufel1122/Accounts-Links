847ab1b6d8385d174050b4d60eab1a6be56a558e
Muhammad Nofil <nofil.khan037@gmail.com>
2023-09-18 17:10:03 +0500

    almost complete

--- account/src/index.jsx
+++ account/src/index.jsx
@@ -1,7 +1,12 @@
 import React from "react";
-import  ReactDOM from "react-dom";
+import ReactDOM from "react-dom";
 import './index.css'
-import image1 from './MyImage.png'; 
+import image1 from './MyImage.png';
+import github from './github.svg';
+import linkedin from './linkedin.svg';
+import instagram from './instagram.svg';
+import facebook from './facebook.svg';
+import whatsapp from './whatsapp.svg';
 
 function Hi() {
   return (<div className="main">
@@ -9,11 +14,20 @@ function Hi() {
       <div className="content">
         <img src={image1} alt="" />
         <h1>Muhammad Naufel</h1>
-        <div className="github"></div>
-        <div className="linkedin"></div>
-        <div className="instagram"></div>
-        <div className="Facebook"></div>
-        <div className="Resume"></div>
+        <div className="github">
+          <p>  <img src={github} alt="" /> Github</p>
+        </div>
+        <div className="linkedin">
+          <p><img src={linkedin} alt="" />Linkedin</p>
+        </div>
+        <div className="instagram">
+          <p><img src={instagram} alt="" />Instagram</p>
+        </div>
+        <div className="Facebook">
+          <p><img src={facebook} alt="" />Facebook</p>
+        </div>
+        <div className="Resume">
+          <p><img src={whatsapp} alt="" />WhatsApp</p></div>
       </div>
     </div>
   </div>)
